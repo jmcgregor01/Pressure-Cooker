0c12012fedad2b839c0015672fdea7a55cc9f6f3
Rizwan <phone.asim@gmail.com>
2018-09-19 10:24:10 +1000

    Small fix

---
+++ website/website/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- <link rel="icon" 
+      type="image/jpg" 
+      href="resources/icon.jpg"> -->
+  <title>HOME PAGE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="styles.css"> -->
+		<link href="style.css" rel="stylesheet">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+
+
+<!-- 
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="assets\style.css" rel="stylesheet">
+		<link 	rel="stylesheet" 
+				href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+				integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+				crossorigin="anonymous">
+		
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+	</head>
+ -->
+
+
+
+
+
+
+<!----------------------------body-------------------->
+<body style="background-image:linear-gradient(to right,white,grey);color:black;">
+<div class="container-fluid">
+<div class="media">
+  <div class="media-left">
+    <img src="resources/citlogo.jpeg" class="media-object" style="width:60px" alt="Image">
+  </div>
+  <div class="media-body">
+  <h1 class="media-heading">PRESSURE COOKER</h1>
+  </div>
+</div>
+<!-------top navigation-------------->
+  <nav class="navbar navbar-inverse"  id = "scrollingHeader">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.html">HOME</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="recipes.html">RECIPES</a></li>
+	  <li><a href="profiles.html">PROFILES</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">EPISODES <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="episode1.html">EPISODE 1</a></li>
+          <li><a href="episode2.html">EPISODE 2</a></li>
+          <li><a href="episode3.html">EPISODE 3</a></li>
+          <li><a href="episode4.html">EPISODE 4</a></li>
+        </ul>
+      </li> 
+	  <li><a href="gallery.html">GALLERY</a></li>
+	  <li><a href="behind.html">BEHIND THE SCENES</a></li>
+      <li><a href="vote.html">VOTE</a></li>
+	  <li><a href="sponsors.html">SPONSORS</a></li>
+    </ul>
+    <form class="navbar-form navbar-right" action="/action_page.php">
+    <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search">
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit">
+        <i class="glyphicon glyphicon-search"></i>
+      </button>
+    </div>
+    </div>
+    </form>
+  </nav> 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	<div class="container-fluid text-center">    
+			  <h3>Our Judges</h3>
+			  <br>
+			  <div class="row">
+				<div class="col-sm-2">
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 1</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 2</p>    
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 3</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 4</p>
+				</div> 
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 5</p>
+				</div>     
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 6</p>
+				</div> 
+			  </div>
+			</div><br>			
+
+
+
+
+			<div class="container-fluid text-center">    
+			  <h3>Our Teams</h3>
+			  <br>
+			  <div class="row">
+				<div class="col-sm-2">
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 1</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 2</p>    
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 3</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 4</p>
+				</div> 
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 5</p>
+				</div>     
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 6</p>
+				</div> 
+			  </div>
+			</div><br>		
+
+
+			
+			
+			<div class="container-fluid text-center">    
+			  <h3>Our Recipes</h3>
+			  <br>
+			  <div class="row">
+				<div class="col-sm-2">
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 1</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 2</p>    
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 3</p>
+				</div>
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 4</p>
+				</div> 
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 5</p>
+				</div>     
+				<div class="col-sm-2"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Partner 6</p>
+				</div> 
+			  </div>
+			</div><br>	
+
+			
+			
+			
+			<div class="container-fluid text-center">    
+			  <h3>What We Do</h3>
+			  <br>
+			  <div class="row">
+				<div class="col-sm-3">
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Current Project</p>
+				</div>
+				<div class="col-sm-3"> 
+				  <img src="resources/image.png" class="img-responsive" style="width:100%" alt="Image">
+				  <p>Project 2</p>    
+				</div>
+				<div class="col-sm-3">
+				  <div class="well">
+				   <p>Some text..</p>
+				  </div>
+				  <div class="well">
+				   <p>Some text..</p>
+				  </div>
+				</div>
+				<div class="col-sm-3">
+				  <div class="well">
+				   <p>Some text..</p>
+				  </div>
+				  <div class="well">
+				   <p>Some text..</p>
+				  </div>
+				</div>  
+			  </div>
+			  <hr>
+			</div>
+	
+	<br><br>
+	
+	
+	
+	
+	
+	
+	
+	
+  </div><!----col-md-4 ends-------->
+  <!-----------------bottom navigation-------------------------------->
+<div>
+  <nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <ul class="nav navbar-nav">
+  <li><a href="#">ADMIN</a></li>
+  </ul>
+  </nav>
+</div>
+
+<!--
+<script>
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("scrollingHeader");
+var stickyheader = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > stickyheader) {
+    header.classList.add("stickyheader");
+  } else {
+    header.classList.remove("stickyheader");
+  }
+}
+</script>
+-->
+
+</body>
+</html>
