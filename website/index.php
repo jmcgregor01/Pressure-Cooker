49169fa802fa4d119efae9cd0c223cafde30488f
Rizwan <phone.asim@gmail.com>
2018-09-19 10:24:11 +1000

    Addded Carousel

--- website/index.php
+++ website/index.php
@@ -9,36 +9,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<!-- <link rel="stylesheet" href="styles.css"> -->
 		<link href="style.css" rel="stylesheet">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
-
-
-
-<!-- 
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link href="assets\style.css" rel="stylesheet">
-		<link 	rel="stylesheet" 
-				href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
-				integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
-				crossorigin="anonymous">
-		
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		
-	</head>
- -->
-
-
-
-
-
 
 <!----------------------------body-------------------->
 <body style="background-image:linear-gradient(to right,white,grey);color:black;">
@@ -84,6 +59,72 @@
     </form>
   </nav> 
 
+	
+	
+	
+				<div class="row">
+				<div class="container-fluid"> 
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					 <!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel" data-slide-to="1"></li>
+							<li data-target="#myCarousel" data-slide-to="2"></li>
+						</ol>
+
+					<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+							<div class="item active">
+								<img src="image\00.png" alt="Los Angeles" style="width:100%; height: 400px;">
+							</div>
+
+							<div class="item">
+								<img src="image\00.jpg" alt="Chicago" style="width:100%; height: 400px;">
+							</div>
+
+							<div class="item">
+								<img src="image\healthyfood_banner.jpg" alt="New york" style="width:100%; height: 400px;">
+							</div>
+						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
