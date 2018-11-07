5930f202d4dc56eadd5ef34505dacb0d53511880
Jacob McGregor <jacob.mcgregor001@gmail.com>
2018-11-07 13:09:02 +1100

    Adding Tarn's changes
    Resolving merge conflicts

--- website/pressure_cooker/index.php
+++ website/pressure_cooker/index.php
@@ -22,67 +22,10 @@
 	<div class="container-fluid">
 
 		<!--Top Navigation Bar-->
-		<nav class="navbar navbar-default navbar-static-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>                        
-					</button>
-
-					<div class="navbar-header mouseOnlogo">
-						<a href="https://cit.edu.au/">
-							<h2 class="citLogo">
-								<span class="ciLogoPressureLogo">Ci</span><span class="tLogo">T</span>
-							</h2>
-
-							<h4 class="pressureCookerLogo">
-								<span class="ciLogoPressureLogo">PRESSURE</span><span class="cookerLogo">COOKER</span>
-							</h4>
-						</a>
-					</div>
-				</div>
-
-				<div class="collapse navbar-collapse" id="myNavbar" style="margin-top: 35px;">
-					<ul class="nav navbar-nav" style="margin-left: 80px;">
-						<li><a href="#"><span style="font-weight: bold; font-size: 15px;">HOME</span></a>
-						</li>
-						<li><a href="#judgesSection"><span style="font-weight: bold; font-size: 15px;">JUDGES</span></a>
-						</li>
-						<li><a href="#teamSection"><span style="font-weight: bold; font-size: 15px;">TEAMS</span></a>
-						</li>
-						<li><a href="#recipesSection"><span style="font-weight: bold; font-size: 15px;">RECIPES</span></a>
-						</li>
-						<li><a href="#mediaSection"><span style="font-weight: bold; font-size: 15px;">WATCH NOW</span></a>
-						</li>
-						<li><a href="#gallerySection"><span style="font-weight: bold; font-size: 15px;">GALLERY</span></a>
-						</li>
-						<li><a href="#citContacts"><span style="font-weight: bold; font-size: 15px;">CONTACT</span></a>
-						</li>
-					</ul>
-
-					<form style="margin-right: 50px;" class="navbar-form navbar-right" action="search.php">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search" name="search">
-						</div>
-
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
-				</div>
-			</div>
-			<!--Ending Top Navigation Bar-->
-		</nav>
-
-		
-
-		
-		
-		
-		
-		
-		
-		
+		<?php
+		include "templates/navigationbar_template.php";
+		?>
+		<!--Ending Top Navigation Bar-->
 			<?php
 			$carousel_query = "SELECT * FROM carousel
 								ORDER BY id DESC";
