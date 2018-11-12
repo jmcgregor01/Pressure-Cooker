81e52e808b66d3902a638af900c4e10b9a448be2
Rizwan <phone.asim@gmail.com>
2018-11-12 16:04:41 +1100

    Making Andrew's map Responsive

--- website/pressure_cooker/index.php
+++ website/pressure_cooker/index.php
@@ -362,9 +362,10 @@
 				</div>
 			</div>
 			<hr>
-
-			<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6513.543193413012!2d149.1329614864318!3d-35.286804613391176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd9d3b3beb2c34d49!2sCanberra+Institute+of+Technology!5e0!3m2!1sen!2sau!4v1539905585501" width="1000" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<div class="embed-responsive embed-responsive-16by9"  style="border-radius: 5px;">
+			<center><iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6513.543193413012!2d149.1329614864318!3d-35.286804613391176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd9d3b3beb2c34d49!2sCanberra+Institute+of+Technology!5e0!3m2!1sen!2sau!4v1539905585501" width="1000" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</center>
+			</div>
 		<!--Ending Contact Us Container-->
 		</div>
 	<!--Ending Body Content-->
