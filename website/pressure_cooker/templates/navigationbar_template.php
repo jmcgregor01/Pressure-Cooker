908351a9f9606dbd1730f0eeab1045165e621319
AndrewJ1993 <andrew.s.jarrett1@gmail.com>
2018-11-14 15:06:50 +1100

    Final commit
    Changes to how videos are saved in the database and outputted in watch now section.
    Other minor changes and edits

--- website/pressure_cooker/templates/navigationbar_template.php
+++ website/pressure_cooker/templates/navigationbar_template.php
@@ -30,7 +30,13 @@
 						</li>
 						<li class="<?php echo $recipe_select; ?>"><a href="recipesLink.php"><span style="font-weight: bold; font-size: 15px;">RECIPES</span></a>
 						</li>
-						<li class="<?php echo $media_select; ?>"><a href="mediaLink.php"><span style="font-weight: bold; font-size: 15px;">WATCH NOW</span></a>
+						<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span style="font-weight: bold; font-size: 15px;">WATCH NOW</span><span class="caret" ></span></a>
+							<ul class="dropdown-menu">
+          						<li class="<?php echo $behind_select; ?>"><a href="mediaLink.php?type=e"><center>EPISODES</center></a></li>
+          						<li class="<?php echo $sponsor_select; ?>"><a href="mediaLink.php?type=j"><center>JUDGES AND HOSTS</center></a></li>
+								<li class="<?php echo $android_select; ?>"><a href="mediaLink.php?type=c"><center>CONTESTANTS</center></a></li>
+          						<li class="<?php echo $about_select; ?>"><a href="mediaLink.php?type=b"><center>BEHIND THE SCENES</center></a></li>
+        					</ul>
 						</li>
 						<li class="<?php echo $gallery_select; ?>"><a href="galleryLink.php"><span style="font-weight: bold; font-size: 15px;">GALLERY</span></a>
 						</li>
