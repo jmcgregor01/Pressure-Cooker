1441a85ef9eae24247ecdf10fe13705348ccf65c
Rizwan <phone.asim@gmail.com>
2018-11-22 12:21:17 +1100

    db file password

--- website/pressure_cooker/admin/config/db.php
+++ website/pressure_cooker/admin/config/db.php
@@ -1,4 +1,4 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "pressure_cooker")
+	$conn = mysqli_connect("localhost", "root", "pass", "pressure_cooker")
 					or die("mySQL server connection failed");
 ?>
